c3956e01c8301ad41a2d4be42626bb12fb0fa026
marcos <marcsalexandr@gmail.com>
2024-01-04 19:48:12 -0300

    Committing changes before updating Angular CLI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { CourseListComponent } from './courses/course-list.component'; // Certifique-se de que o caminho está correto
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CourseListComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
---
+++ src/app/courses/course-list.component.ts
@@ -0,0 +1,13 @@
+import { Component } from "@angular/core";
+
+
+@Component({
+    selector: 'app-course-list',
+    templateUrl:'./course-list.component.html'
+
+})
+export class CourseListComponent {
+
+    
+
+}
